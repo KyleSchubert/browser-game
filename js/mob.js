d9deeabfd99ee9fbec5424580ab6df95a6b16a0e
Kyle Schubert <jedikyle5@gmail.com>
2020-12-26 00:29:37 +0000

    Mobs can spawn and die They have animations for being alive and dying They can be spawned from the list They can be killed with the button They can be spawned with the button

--- js/mob.js
+++ js/mob.js
@@ -1,13 +1,48 @@
-function spawn(fromList=false) {
+function getMob(fromList=false) {
     if (fromList) {
         if ($('.easySelected').length > 0) {
-            monster = $('.easySelected').html().toLowerCase();
+            mob = $('.easySelected').html().toLowerCase();
         }
         else {
-            monster = 'tino';
+            mob = 'tino';
         }
     }
     else {
-        monster = knownMobs[Math.floor(Math.random() * knownMobs.length)];
+        mob = knownMobs[Math.floor(Math.random() * knownMobs.length)];
+    }
+    return mob
+}
+
+function spawn(mob=getMob(true)) {
+    $('#mobArea').append(mobGifSetup(mob))
+}
+
+function mobGifSetup(name) { // name in any case
+    name = name.toLowerCase();
+    gif = '/mob/alive/' + name + '.gif';
+    var img = new Image();
+    img.classList = ["mob clickable"];
+    img.src = gif;
+    img.value = name;
+    img.setAttribute('draggable', false);
+    return img;
+}
+
+function mobDie(origin='') {
+    if (!origin) {
+        target = $('#mobArea img:not(.mobDying)').last();
+    }
+    else {
+        target = this; // it will be activated by clicking the mob so that should catch it
+    }
+    if (target.length > 0) {
+        mobName = target.val();
+        target.attr('src', '/mob/dead/' + mobName + '.gif')
+        $(target).on('transitionend webkitTransitionEnd oTransitionEnd', function () {
+            target.remove()
+        });
+        target.css('transition-duration', mobDeathDuration[mobName].toString() + 'ms')
+        target.addClass('mobDying')
     }
 }
+
--- js/mobData.js
+++ js/mobData.js
@@ -1,1 +1,4 @@
-var knownMobs = ['tino', 'orange mushroom', 'mushmom'];
+var knownMobs = ['tino', 'orange mushroom', 'mushmom', 'mutant ribbon pig'];
+
+// found using   http://gifduration.konstochvanligasaker.se/     (these are in milliseconds)
+var mobDeathDuration = {'tino': 1560, 'orange mushroom': 660, 'mushmom': 1100, 'mutant ribbon pig': 2160};
