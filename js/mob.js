74cbcb7efdbfae26b71843644f83cd503d9d704d
Shrifli <jedikyle5@gmail.com>
2021-11-07 23:47:05 -0500

    - Began trying to swap over to png spritesheets from gifs in order to have independent frames of the same gif on different monsters

--- js/mob.js
+++ js/mob.js
@@ -14,36 +14,35 @@ function getMob(fromList=false) {
 }
 
 function spawn(mob=getMob(true)) {
-    $('#mobArea').append(mobGifSetup(mob));
+    let madeMob = mobGifSetup(mob);
+    mobMove(madeMob);
+    $('#mobArea').append(madeMob);
 }
 
 function mobGifSetup(name) { // name in any case
     name = name.toLowerCase();
-    gif = '/mob/alive/' + name + '.gif';
-    let img = new Image();
-    img.classList = ['mob clickable'];
-    img.src = gif;
-    img.value = name;
-    img.setAttribute('draggable', false);
-    img.setAttribute('hp', Math.ceil(Math.random() * 34) + 90); // temporary example for HP
-    img.setAttribute('maxHP', $(img).attr('hp')); // temporary example for maxHP
-    $('#mobHP').text(''.concat($(img).attr('hp'), ' / ', $(img).attr('maxHP')));
-    $(img).on('load', () => {
-        $(img).css('left', '-=' + img.width/2);
-        $(img).off('load');
-    })
-    $(img).click(function() {// MOBS TAKE DAMAGE ON CLICK
-        newHP = $(this).attr('hp') - rollDamageToMob();
+    let div = document.createElement('div');
+    div = $(div);
+    div.val(name);
+    mobSetAnimation(div, 'alive');
+    div.css('left', 540 - mobDimensions[name]['alive'][0]/2 + 'px')
+    div.addClass('mob clickable');
+    div.attr('draggable', false);
+    div.attr('hp', Math.ceil(Math.random() * 34) + 90); // temporary example for HP
+    div.attr('maxHP', $(div).attr('hp')); // temporary example for maxHP
+    $('#mobHP').text(''.concat(div.attr('hp'), ' / ', div.attr('maxHP')));
+    div.on('click', (event) => {// MOBS TAKE DAMAGE ON CLICK
+        newHP = $(event.currentTarget).attr('hp') - rollDamageToMob();
         if (newHP < 0) {
             newHP = 0;
         }
-        $('#mobHP').text(''.concat(newHP, ' / ', $(img).attr('maxHP')));
+        $('#mobHP').text(''.concat(newHP, ' / ', $(event.currentTarget).attr('maxHP')));
         if (newHP <= 0) {
-            mobDie(this);
+            mobDie(event.currentTarget);
         }
-        $(this).attr('hp', newHP);
+        $(event.currentTarget).attr('hp', newHP);
     });
-    return img;
+    return div;
 }
 
 function rollDamageToMob(skill='') {
@@ -93,15 +92,15 @@ function rollDamageToMob(skill='') {
 
 function mobDie(origin='') {
     if (!origin) {
-        target = $('#mobArea img:not(.mobDying)').last();
+        target = $('#mobArea div:not(.mobDying)').last();
     }
     else {
         target = $(origin); // it will be activated by clicking the mob so that should catch it
     }
     if (target.length > 0 || target.is('img')) {
         target.css('pointer-events', 'none');
-        mobName = target.val();
-        target.attr('src', '/mob/dead/' + mobName + '.gif');
+        let mobName = target.val();
+        mobSetAnimation(target, 'dead')
         target.css('transition-duration', mobDeathDuration[mobName].toString() + 'ms');
         target.addClass('mobDying');
 
@@ -120,25 +119,13 @@ function mobDie(origin='') {
 }
 
 MAX_MOBS = 1;
-$(document).ready(function() {
+$(document).ready( () => {
     spawn(getMob())
-    setInterval(function() {
+    setInterval(() => {
         if ($('.mob').length < MAX_MOBS) {
             console.log("Spawning a mob");
             spawn(getMob());
         }}, 14000);
-})
-
-$(document).on('transitionend webkitTransitionEnd oTransitionEnd', '.mob', function(event) { // part of the mob death effect
-    if ($(event.currentTarget).hasClass('mobDying')) {
-        $(event.currentTarget).remove();
-    }
-});
-
-$(document).on('animationend webkitAnimationEnd oAnimationEnd', '.mobMoving', function(event) { // part of the mob death effect
-    $(event.currentTarget).removeClass('mobMoving');
-    if (!$(event.currentTarget).hasClass('mobDying'))
-        $(event.currentTarget).attr('src', '/mob/alive/' + $(event.currentTarget).val() + '.gif');
 })
 
 function gainTextStreamAdd(text) {
@@ -153,34 +140,75 @@ $(document).on('animationend webkitAnimationEnd oAnimationEnd', '.fadeToGone', f
     $(event.currentTarget).remove();
 });
 
-function mobMove(index) {
+function mobMove(mob) {
+    mob = $(mob);
     setTimeout(() => {
-        let mob = $('.mob:eq(' + index + ')');
-        $(mob).on('load', () => {
-            let movingLeft = Boolean(randomIntFromInterval(0, 1));
-            let distance = mobMoveDuration[mob.val()] * randomIntFromInterval(1, 6) * .05;
-            let final = 0;
-            let duration = distance / .05;
-            console.log('Movingleft: %s, distance: %s, duration: %s', movingLeft, distance, duration)
-            if (movingLeft) { 
-                final = parseInt(mob.css('left'))-distance;
-            }
-            else {
-                final = parseInt(mob.css('left'))+distance;
-            }
-            mob.css('animation-duration', duration + 'ms');
-            mob.css('--currentLeft', mob.css('left'));
-            mob.css('--finalLeft',  final + 'px');
-            mob.addClass('mobMoving');
-            if (movingLeft) { 
-                mob.css('transform', 'scaleX(1)');
-            }
-            else {
-                mob.css('transform', 'scaleX(-1)');
-            }
-            mob.css('left', final + 'px');
-            mob.off('load');
-        })
-        mob.attr('src', '/mob/move/' + mob.val() + '.gif');
-    }, Math.random()*1000);
+        mobSetAnimation(mob, 'move');
+        let movingLeft = Boolean(randomIntFromInterval(0, 1));
+        let distance = mobMoveDuration[mob.val()] * randomIntFromInterval(1, 6) * .05;
+        let final = 0;
+        let duration = distance / .05;
+        console.log('Movingleft: %s, distance: %s, duration: %s', movingLeft, distance, duration)
+        if (movingLeft) { 
+            final = parseInt(mob.css('left'))-distance;
+        }
+        else {
+            final = parseInt(mob.css('left'))+distance;
+        }
+        mob.css('animation-duration', duration + 'ms');
+        mob.css('--currentLeft', mob.css('left'));
+        mob.css('--finalLeft',  final + 'px');
+        mob.addClass('mobMoving');
+        if (movingLeft) { 
+            mob.css('transform', 'scaleX(1)');
+        }
+        else {
+            mob.css('transform', 'scaleX(-1)');
+        }
+        mob.css('left', final + 'px');
+    }, randomIntFromInterval(4217, 10203));
+}
+
+function mobSetAnimation(mob, status) {
+    mob = $(mob);
+    sprites = '/mob/' + status + '/' + mob.val() + '.png';
+    mob.attr('status', status)
+    mob.css('background-image', 'url(' + sprites + ')');
+    console.log(mob)
+    console.log(mob.val())
+    console.log(mobDimensions[mob.val()])
+    console.log(status)
+    mob.css('width', mobDimensions[mob.val()][status][0] + 'px')
+    mob.css('height', mobDimensions[mob.val()][status][1] + 'px')
+    //mob.css('left', '-=' + mobDimensions[name]['alive'][0]/2)
+}
+
+function someAnimate(mob, frame=0) {
+    mob = $(mob);
+    let status = mob.attr('status');
+    let durationSource = [];
+    mob.css('background-position-x', '-=' + mob.width());
+    if (status == 'alive') {
+        durationSource = mobAliveFrameDurations[mob.val()];
+    }
+    else if (status == 'dead') {
+        durationSource = mobDeadFrameDurations[mob.val()];
+    }
+    else if (status == 'move') {
+        durationSource = mobMoveFrameDurations[mob.val()];
+    }
+    if (frame >= durationSource.length) {
+        if (mob.hasClass('mobDying')) {
+            $(mob).remove();
+        }
+        if (mob.attr('queued')) {
+            status = mob.attr('queued');
+        }
+        mob.css('background-position-x', '0px')
+        frame = 0;
+    }
+    console.log(durationSource)
+    setTimeout(() => {
+        someAnimate(mob, frame+1)
+    }, durationSource[frame]);
 }
--- js/mobData.js
+++ js/mobData.js
@@ -3,7 +3,12 @@ var knownMobs = ['tino', 'orange mushroom', 'mushmom', 'mutant ribbon pig'];
 
 // found using   https://ezgif.com/maker     (these are in milliseconds)
 var mobDeathDuration = {'tino': 1560, 'orange mushroom': 660, 'mushmom': 1100, 'mutant ribbon pig': 2160};
-var mobMoveDuration = {'tino': 850, 'orange mushroom': 480, 'mushmom': 1200, 'mutant ribbon pig': 960}
+var mobMoveDuration = {'tino': 850, 'orange mushroom': 480, 'mushmom': 1200, 'mutant ribbon pig': 960};
+
+var mobAliveFrameDurations = {'mushmom': [250], 'mutant ribbon pig': [180, 180, 180, 180, 180, 180], 'orange mushroom': [180, 180], 'tino': [210, 210, 210, 210]};
+var mobDeadFrameDurations = {'mushmom': [200, 150, 150, 150, 150, 300], 'mutant ribbon pig': [120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120], 'orange mushroom': [180, 180, 300], 'tino': [120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120]};
+var mobMoveFrameDurations = {'mutant ribbon pig': [120, 120, 120, 120, 120, 120, 120, 120], 'orange mushroom': [180, 120, 180], 'tino': [0, 150, 150, 150, 150, 100], 'mushmom': [500, 100, 100, 300, 100, 100]};
+var mobDimensions = {'tino': {'alive': [33, 34], 'dead': [74, 36], 'move': [37, 41]}};
 
 var mobDropPools = {
    'tino': ['sub10Gear'],
