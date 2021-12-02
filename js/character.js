d3b0de06aec761c5d7b241b57a3bbf9001966b81
Shrifli <jedikyle5@gmail.com>
2021-12-02 17:13:08 -0500

    - Changed the amount of skill points per level to 3 and moved some code

--- js/character.js
+++ js/character.js
@@ -57,7 +57,7 @@ var character = {
                 if (character.info.level < 10) {
                     whichTierGetsSkillPoints = 0;
                 }
-                character.info.skillPoints[whichTierGetsSkillPoints]++;
+                character.info.skillPoints[whichTierGetsSkillPoints] += 3;
                 if (whichTierGetsSkillPoints == document.getElementsByClassName('selectedSkillTab')[0].getAttribute('value')) {
                     document.getElementById('skillPoints').innerHTML = character.info.skillPoints[whichTierGetsSkillPoints];
                 }
@@ -162,31 +162,4 @@ function updateOneCharacterDisplay(subject) {
 
 const classMainStat = {
     'Kaiser': 'strength'
-};
-
-const skillsPerClass = {
-    'Kaiser': [[61001000], [], [], [], [], []]
-};
-
-const classSkills = {
-    61001000: {
-        className: 'Kaiser',
-        skillName: 'Dragon Slash',
-        tier: 1,
-        maxLevel: 20,
-        type: 'attackSequence',
-        reuseWaitTime: 320,
-        delays: [90,90,90,90],
-        attackSequence: {
-            61001000: {damage: 0.50, lines: 3, targets: 8, scaling: {damage: 0.01}, dimensions: [283,167], next: 61001004},
-            61001004: {damage: 0.30, lines: 5, targets: 6, scaling: {damage: 0.01}, dimensions: [363,129], next: 61001005},
-            61001005: {damage: 0.40, lines: 5, targets: 6, scaling: {damage: 0.01}, dimensions: [335,209], next: 61001000}
-        },
-        finalForm: {damage: 1.40, lines: 5, targets: 8, scaling: {damage: 0.01}},
-        hitDelays: [90,90,90,90,90,90],
-        hitDimensions: [187, 131],
-        description: 'Mash the attack key to whip enemies in front of you up to 3 times. Can be used with basic attack key while in Final Form.',
-        requirementText: 'Level 20 required to learn Dragon Slash I.',
-        hitDescriptions: ['1-hit: Attack up to 8 enemies for {damage}% damage 3 times', '2-hit: Up to 6 enemies attack for {damage}% damage 5 times', '3-hit: Up to 6 enemies attack for {damage}% damage 5 times', 'Final Form: Up to 8 enemies attack for {damage}% damage 5 times, Up to 10 enemies attack 6 times after 4th job']
-    }
 };
