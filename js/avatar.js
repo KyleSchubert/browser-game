baf49d9cbea4ddf08b36f85ae527f0ea5104c06f
Shrifli <jedikyle5@gmail.com>
2021-12-21 15:50:56 -0500

    - Started the new avatar

--- js/avatar.js
+++ js/avatar.js
@@ -30,3 +30,59 @@ $(() => {
         lastAvatarHeight = $(this).height();
     });
 });
+
+function testing() {
+    let face = 20000;
+    let head = 12000;
+    let body = 2000;
+    let ids = [face, head, body];
+    let faceState = 'default';
+    let headState  = 'front';
+    let bodyState  = 'stand1';
+    let states = [faceState, headState, bodyState];
+    for (let i=0; i<ids.length; i++) {
+        addBodyPartToAvatar(ids[i], states[i]);
+    }
+}
+
+function addBodyPartToAvatar(id, state) {
+    let appendLocation = document.getElementById('avatarAreaNew');
+    let frames = bodyData[id][state];
+    let initialFrame = frames[0];
+    initialFrame.forEach((part) => {
+        let fileDir = './item/' + id + '/' + part[0] + '.png';
+        let coordsForPart = part[1];
+        let partDiv = document.createElement('div');
+        let partDimensions = bodyDimensions[id][part[0]][1];
+        let staticPartDimensions = bodyOffsets[id][part[0]]; // these never change for any frame
+        switch(id) {
+            case 2000: // body
+                partDiv.style.zIndex = 1;
+                staticPartDimensions[0] += -10;
+                staticPartDimensions[1] += 3;
+                break;
+            case 12000: // head
+                partDiv.style.zIndex = 2;
+                staticPartDimensions[0] += -15;
+                staticPartDimensions[1] += -15;
+                break;
+            case 20000: // face
+                partDiv.style.zIndex = 3;
+                staticPartDimensions[0] += -8;
+                staticPartDimensions[1] += 10;
+                break;
+        }
+        partDiv.classList = ['avatarPart'];
+        partDiv.style.width = partDimensions[0] + 'px';
+        partDiv.style.height = partDimensions[1] + 'px';
+        partDiv.style.backgroundImage = 'url(' + fileDir + ')';
+        partDiv.style.backgroundPosition = '-' + coordsForPart[0] + 'px -' + coordsForPart[1] + 'px';
+        partDiv.style.left = staticPartDimensions[0] + 'px';
+        partDiv.style.top = staticPartDimensions[1] + 'px';
+        appendLocation.appendChild(partDiv);
+    });
+}
+
+$(() => {
+    testing();
+});
