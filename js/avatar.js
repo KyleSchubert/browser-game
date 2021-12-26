95899d5f6106a50ea7031ee3272eb20e7542d513
Shrifli <jedikyle5@gmail.com>
2021-12-26 00:53:28 -0500

    - Changed avatar mapping formula to someone else's formula

--- js/avatar.js
+++ js/avatar.js
@@ -35,30 +35,29 @@ function testing() {
     let body = 2000;
     let head = 12000;
     let face = 20000;
-    let ids = [body, head, face];
+    let ids = [head, body, face];
     let faceState = 'default';
     let headState  = 'stand1';
     let bodyState  = 'stand1';
-    let states = [bodyState, headState, faceState];
     for (let i=0; i<ids.length; i++) {
         addBodyPartsToAvatar(ids[i]);
         //addBodyPartToAvatar(ids[i], states[i]);
     }
-    makeTestPixel(neck[0], neck[1]);
-    makeTestPixel(neck[0]-4, neck[1]-19);
-    makeTestPixel(neck[0]-4, neck[1]+11);
+    makeTestPixel(neck[0], neck[1], 'blue');
+    makeTestPixel(neck[0]-4, neck[1]-19, 'blue');
+    makeTestPixel(neck[0]-4, neck[1]+11, 'blue');
     cyclicAvatarAnimate(body, bodyState);
     cyclicAvatarAnimate(head, headState);
     cyclicAvatarAnimate(face, faceState);
 }
 
-function makeTestPixel(x, y) {
+function makeTestPixel(x, y, color='red') {
     let appendLocation = document.getElementById('avatarAreaNew');
     let pixel = document.createElement('div');
     pixel.style.position = 'absolute';
     pixel.style.left = x + 'px';
     pixel.style.top = y + 'px';
-    pixel.style.backgroundColor = 'red';
+    pixel.style.backgroundColor = color;
     pixel.style.width = '1px';
     pixel.style.height = '1px';
     pixel.style.zIndex = '999';
@@ -79,35 +78,15 @@ function cyclicAvatarAnimate(id, state, reverse=false, frame=0) { // the part ne
         }
         let partXOffset = part[0][1];
         let partDiv = allAvatarParts[id][partName];
-        let staticPartDimensions = bodyOffsets[id][partName][0]; // these never change for any frame
-        let mapTo = part[1];
-        let origin = part[2];
-        if (mapTo[0] == 'neck' || mapTo[0] == 'brow' || mapTo[0] == 'navel') {
-            let value = [0, 0];
-            if (mapTo[0] == 'brow') {
-                value[0] -= 4;
-                value[1] -= 20;
-            }
-            else if (mapTo[0] == 'navel') {
-                value[0] -= 4;
-                value[1] += 11;
-            }
-            //console.log('NECK STUFF!  ' + neck);
-            //console.log(2*origin[0] - staticPartDimensions[0] + mapTo[1][0]);
-            //console.log(2*origin[1] - staticPartDimensions[1] + mapTo[1][1]);
-            //let changeX = neck[0] + value[0] - (2*origin[0] - staticPartDimensions[0] + mapTo[1][0]);
-            //let changeY = neck[1] + value[1] - (2*origin[1] - staticPartDimensions[1] + mapTo[1][1]);
-            let relativeLocationOfTargetX = origin[0] + mapTo[1][0];
-            let relativeLocationOfTargetY = origin[1] + mapTo[1][1];
-            let changeX = neck[0] + value[0] - relativeLocationOfTargetX;
-            let changeY = neck[1] + value[1] - relativeLocationOfTargetY;
-            partDiv.style.left = changeX + 'px';
-            partDiv.style.top = changeY + 'px';
-        }
-        
         partDiv.style.backgroundPositionX = '-' + partXOffset + 'px';
         partDiv.style.visibility = 'visible';
+        console.log('TRIED TO POSITION ' + partName + ' at:');
+        let offsets = positionOneAvatarPart(part);
+        console.log(offsets);
+        partDiv.style.left = offsets[0] + 'px';
+        partDiv.style.top = offsets[1] + 'px';
     });
+    return;
     if (frame == bodyData[id][state].length-1) {
         setTimeout(() => {
             cyclicAvatarAnimate(id, state, true, frame-1);
@@ -130,19 +109,11 @@ function cyclicAvatarAnimate(id, state, reverse=false, frame=0) { // the part ne
     }
 }
 
-function originFinder() {
-    let x = 0;
-    let y = 0;
-    makeTestPixel(x, y);
-    return;
-}
-
 var allAvatarParts = {};
-var neck = [21, 34];
+//var neck = [21, 34];
 var avatarNavel = 1;
 function addBodyPartsToAvatar(id) {
     let appendLocation = document.getElementById('avatarAreaNew');
-    //let initialFrame = frames[0];
     let doneParts = [];
     Object.keys(bodyData[id]).forEach((state) => {
         bodyData[id][state].forEach((frame) => {
@@ -156,48 +127,13 @@ function addBodyPartsToAvatar(id) {
                 let partXOffset = part[0][1];
                 let partDiv = document.createElement('div');
                 let partDimensions = bodyDimensions[id][partName][1];
-                let staticPartDimensions = bodyOffsets[id][partName][0]; // these never change for any frame
-                let mapTo = part[1];
-                let origin = part[2];
-                console.log(partName);
-                console.log(state);
-                console.log(mapTo);
-                //console.log(partName);
-                //console.log(mapTo);
-                if (mapTo[0] == 'neck' || mapTo[0] == 'brow' || mapTo[0] == 'navel') {
-                    let value = [0, 0];
-                    if (mapTo[0] == 'brow') {
-                        value[0] -= 4;
-                        value[1] -= 20;
-                    }
-                    else if (mapTo[0] == 'navel') {
-                        value[0] -= 4;
-                        value[1] += 11;
-                    }
-                    //console.log('NECK STUFF!  ' + neck);
-                    //console.log(2*origin[0] - staticPartDimensions[0] + mapTo[1][0]);
-                    //console.log(2*origin[1] - staticPartDimensions[1] + mapTo[1][1]);
-                    //let changeX = neck[0] + value[0] - (2*origin[0] - staticPartDimensions[0] + mapTo[1][0]);
-                    //let changeY = neck[1] + value[1] - (2*origin[1] - staticPartDimensions[1] + mapTo[1][1]);
-                    let relativeLocationOfTargetX = origin[0] + mapTo[1][0];
-                    let relativeLocationOfTargetY = origin[1] + mapTo[1][1];
-                    console.log(relativeLocationOfTargetX);
-                    console.log(relativeLocationOfTargetY);
-                    let changeX = neck[0] + value[0] - relativeLocationOfTargetX;
-                    let changeY = neck[1] + value[1] - relativeLocationOfTargetY;
-                    partDiv.style.left = changeX + 'px';
-                    partDiv.style.top = changeY + 'px';
-                }
+                console.log('TRIED TO POSITION ' + partName + ' at:');
+                let offsets = positionOneAvatarPart(part);
+                console.log(offsets);
+                partDiv.style.left = offsets[0] + 'px';
+                partDiv.style.top = offsets[1] + 'px';
                 if (id == 2000) {
                     partDiv.style.zIndex = 1;
-                    let xSpot = staticPartDimensions[0] + -10;
-                    let ySpot = staticPartDimensions[1] + 3;
-                    if (partName == 'body' && state == 'stand1') {
-                        //neck = [origin[0] + mapTo[1][0] + xSpot, origin[1] + mapTo[1][1] + ySpot];
-                        //console.log('The neck is at  (left, top):  ' + neck[0] + ', ' + neck[1]);
-                        //partDiv.style.left = xSpot + 'px';
-                        //partDiv.style.top = ySpot + 'px';
-                    }
                 }
                 else if (id == 12000) {
                     if (part[0][0] == 'accessoryOverHair') {
@@ -217,12 +153,85 @@ function addBodyPartsToAvatar(id) {
                 if (!(id in allAvatarParts)) {
                     allAvatarParts[id] = {};
                 }
-                //makeTestPixel(origin[0], origin[1]);
+                makeTestPixel(offsets[0], offsets[1]);
                 appendLocation.appendChild(partDiv);
                 allAvatarParts[id][partName] = partDiv;
             });
         });
     });
+}
+
+// I'LL REMOVE WHAT I DONT USE (like normal)
+var brow = [0, 0];
+var headBrow = [0, 0];
+var headNeck = [0, 0];
+var neck = [0, 0];
+var bodyNeck = [0, 0];
+var bodyNavel = [0, 0];
+var navel = [0, 0];
+var armNavel = [0, 0];
+var armHand = [0, 0];
+var hand = [0, 0];
+var handMove = [0, 0];
+var leftHandMove = [0, 0];
+function positionOneAvatarPart(part) { // converted to JS from pascal and slightly changed (from github user Elem8100's https://github.com/Elem8100/MapleStory-GM-Client/blob/1dd68e134e84fba54937c21d9889eff4c63dbe94/Src/MapleCharacter.pas)
+    console.log('NECK');
+    console.log(neck);
+    let offsets = [0, 0];  // [Left, Top] like everything else
+    let origin = part[2];
+    let maps = part[1];
+    let mapSpots = Object.keys(maps);
+    let partName = part[0][0]; // I'm assuming this is what they used in place of these -> example:   if (Image == 'arm') { ... }
+    if (mapSpots.includes('brow')) {
+        brow[0] = -maps['brow'][0];
+        brow[1] = -maps['brow'][1];
+        if (partName == 'head') {
+            headBrow = brow;
+        }
+        offsets[0] = origin[0] + headNeck[0] - bodyNeck[0] - headBrow[0] + brow[0];
+        offsets[1] = origin[1] + headNeck[1] - bodyNeck[1] - headBrow[1] + brow[1];
+    }
+    if (mapSpots.includes('neck')) {
+        neck[0] = -maps['neck'][0];
+        neck[1] = -maps['neck'][1];
+        if (partName == 'body') {
+            bodyNeck = neck;
+        }
+        if (partName == 'head') {
+            headNeck = neck;
+        }
+    }
+    if (mapSpots.includes('hand')) {
+        hand[0] = -maps['hand'][0];
+        hand[1] = -maps['hand'][1];
+        if (partName == 'arm') {
+            armHand = hand;
+        }
+        offsets[0] = origin[0] + hand[0] + armNavel[0] - armHand[0] - bodyNavel[0];
+        offsets[1] = origin[1] + hand[1] + armNavel[1] - armHand[1] - bodyNavel[1];
+    }
+    if (mapSpots.includes('handMove')) {
+        handMove[0] = -maps['handMove'][0];
+        handMove[1] = -maps['handMove'][1];
+        if (partName == 'lHand') {
+            leftHandMove = handMove;
+        }
+        offsets[0] = -(origin[0] + handMove[0] - leftHandMove[0]);
+        offsets[1] = -(origin[1] + handMove[1] - leftHandMove[1]);
+    }
+    if (mapSpots.includes('navel')) {
+        navel[0] = -maps['navel'][0];
+        navel[1] = -maps['navel'][1];
+        if (partName == 'arm') {
+            armNavel = navel;
+        }
+        if (partName == 'body') {
+            bodyNavel = navel;
+        }
+        offsets[0] = origin[0] + navel[0] - bodyNavel[0];
+        offsets[1] = origin[1] + navel[1] - bodyNavel[1];
+    }
+    return offsets;
 }
 
 $(() => {
