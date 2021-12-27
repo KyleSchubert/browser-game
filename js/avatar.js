bd15c6e221b5109eeedef5fdbce1443805ec50a1
Shrifli <jedikyle5@gmail.com>
2021-12-26 19:25:02 -0500

    - New working avatar animator

--- js/avatar.js
+++ js/avatar.js
@@ -43,15 +43,12 @@ function testing() {
         addBodyPartsToAvatar(ids[i]);
         //addBodyPartToAvatar(ids[i], states[i]);
     }
-    makeTestPixel(neck[0], neck[1], 'blue');
-    makeTestPixel(neck[0]-4, neck[1]-19, 'blue');
-    makeTestPixel(neck[0]-4, neck[1]+11, 'blue');
     cyclicAvatarAnimate(body, bodyState);
     cyclicAvatarAnimate(head, headState);
     cyclicAvatarAnimate(face, faceState);
 }
 
-function makeTestPixel(x, y, color='red') {
+function makeTestPixel(x, y, color='red', name='') {
     let appendLocation = document.getElementById('avatarAreaNew');
     let pixel = document.createElement('div');
     pixel.style.position = 'absolute';
@@ -61,6 +58,7 @@ function makeTestPixel(x, y, color='red') {
     pixel.style.width = '1px';
     pixel.style.height = '1px';
     pixel.style.zIndex = '999';
+    pixel.setAttribute('name', name);
     appendLocation.appendChild(pixel);
 }
 
@@ -68,7 +66,14 @@ function cyclicAvatarAnimate(id, state, reverse=false, frame=0) { // the part ne
     if (bodyData[id][state].length == 1) {
         let part = bodyData[id][state][frame][0];
         let partDiv = allAvatarParts[id][part[0][0]];
+        let partName = part[0][0];
+        let partXOffset = part[0][1];
+        partDiv.style.backgroundPositionX = '-' + partXOffset + 'px';
         partDiv.style.visibility = 'visible';
+        console.log('TRIED TO POSITION ' + partName + ' at:');
+        let offsets = positionOneAvatarPart(part,id);
+        console.log(offsets);
+        makeTestPixel(offsets[0], offsets[1], 'red', partName);
         return;
     }
     bodyData[id][state][frame].forEach((part) => {
@@ -81,12 +86,12 @@ function cyclicAvatarAnimate(id, state, reverse=false, frame=0) { // the part ne
         partDiv.style.backgroundPositionX = '-' + partXOffset + 'px';
         partDiv.style.visibility = 'visible';
         console.log('TRIED TO POSITION ' + partName + ' at:');
-        let offsets = positionOneAvatarPart(part);
+        let offsets = positionOneAvatarPart(part, id);
         console.log(offsets);
+        makeTestPixel(offsets[0], offsets[1], 'red', partName);
         partDiv.style.left = offsets[0] + 'px';
         partDiv.style.top = offsets[1] + 'px';
     });
-    return;
     if (frame == bodyData[id][state].length-1) {
         setTimeout(() => {
             cyclicAvatarAnimate(id, state, true, frame-1);
@@ -111,7 +116,6 @@ function cyclicAvatarAnimate(id, state, reverse=false, frame=0) { // the part ne
 
 var allAvatarParts = {};
 //var neck = [21, 34];
-var avatarNavel = 1;
 function addBodyPartsToAvatar(id) {
     let appendLocation = document.getElementById('avatarAreaNew');
     let doneParts = [];
@@ -128,7 +132,7 @@ function addBodyPartsToAvatar(id) {
                 let partDiv = document.createElement('div');
                 let partDimensions = bodyDimensions[id][partName][1];
                 console.log('TRIED TO POSITION ' + partName + ' at:');
-                let offsets = positionOneAvatarPart(part);
+                let offsets = positionOneAvatarPart(part, id);
                 console.log(offsets);
                 partDiv.style.left = offsets[0] + 'px';
                 partDiv.style.top = offsets[1] + 'px';
@@ -153,12 +157,30 @@ function addBodyPartsToAvatar(id) {
                 if (!(id in allAvatarParts)) {
                     allAvatarParts[id] = {};
                 }
-                makeTestPixel(offsets[0], offsets[1]);
                 appendLocation.appendChild(partDiv);
                 allAvatarParts[id][partName] = partDiv;
             });
         });
     });
+}
+
+function getPartType(itemId) {
+    let partType = '';
+    switch (Math.floor(itemId / 10000)) {
+        case 0:
+            partType = 'body';
+            break;
+        case 1:
+            partType = 'head';
+            break;
+        case 2, 5:
+            partType = 'face';
+            break;
+        case 3, 4, 6:
+            partType = 'hair';
+            break;
+    }
+    return partType;
 }
 
 // I'LL REMOVE WHAT I DONT USE (like normal)
@@ -174,62 +196,75 @@ var armHand = [0, 0];
 var hand = [0, 0];
 var handMove = [0, 0];
 var leftHandMove = [0, 0];
-function positionOneAvatarPart(part) { // converted to JS from pascal and slightly changed (from github user Elem8100's https://github.com/Elem8100/MapleStory-GM-Client/blob/1dd68e134e84fba54937c21d9889eff4c63dbe94/Src/MapleCharacter.pas)
-    console.log('NECK');
-    console.log(neck);
+function positionOneAvatarPart(part, itemId) { // converted to JS from pascal and slightly changed (from github user Elem8100's https://github.com/Elem8100/MapleStory-GM-Client/blob/1dd68e134e84fba54937c21d9889eff4c63dbe94/Src/MapleCharacter.pas)
+    //console.log('NECK');
+    //console.log(neck);
     let offsets = [0, 0];  // [Left, Top] like everything else
     let origin = part[2];
     let maps = part[1];
     let mapSpots = Object.keys(maps);
     let partName = part[0][0]; // I'm assuming this is what they used in place of these -> example:   if (Image == 'arm') { ... }
+    console.log('--------' + partName + '---------');
     if (mapSpots.includes('brow')) {
-        brow[0] = -maps['brow'][0];
-        brow[1] = -maps['brow'][1];
+        brow[0] = maps['brow'][0];
+        brow[1] = maps['brow'][1];
         if (partName == 'head') {
-            headBrow = brow;
+            //headBrow = brow;
+            headBrow[0] = brow[0];
+            headBrow[1] = brow[1];
         }
-        offsets[0] = origin[0] + headNeck[0] - bodyNeck[0] - headBrow[0] + brow[0];
-        offsets[1] = origin[1] + headNeck[1] - bodyNeck[1] - headBrow[1] + brow[1];
+        console.log(origin + '   ' + headNeck + '   ' + bodyNeck + '   ' + headBrow + '   ' + brow);
+        offsets[0] = -(origin[0] + headNeck[0] - bodyNeck[0] - headBrow[0] + brow[0]);
+        offsets[1] = -(origin[1] + headNeck[1] - bodyNeck[1] - headBrow[1] + brow[1]);
     }
     if (mapSpots.includes('neck')) {
-        neck[0] = -maps['neck'][0];
-        neck[1] = -maps['neck'][1];
+        neck[0] = maps['neck'][0];
+        neck[1] = maps['neck'][1];
         if (partName == 'body') {
-            bodyNeck = neck;
+            bodyNeck[0] = neck[0];
+            bodyNeck[1] = neck[1];
         }
         if (partName == 'head') {
-            headNeck = neck;
+            headNeck[0] = neck[0];
+            headNeck[1] = neck[1];
         }
     }
     if (mapSpots.includes('hand')) {
-        hand[0] = -maps['hand'][0];
-        hand[1] = -maps['hand'][1];
+        hand[0] = maps['hand'][0];
+        hand[1] = maps['hand'][1];
         if (partName == 'arm') {
-            armHand = hand;
+            armHand[0] = hand[0];
+            armHand[1] = hand[1];
         }
-        offsets[0] = origin[0] + hand[0] + armNavel[0] - armHand[0] - bodyNavel[0];
-        offsets[1] = origin[1] + hand[1] + armNavel[1] - armHand[1] - bodyNavel[1];
+        console.log(origin + '   ' + hand + '   ' + armNavel + '   ' + armHand + '   ' + bodyNavel);
+        offsets[0] = -(origin[0] + hand[0] + armNavel[0] - armHand[0] - bodyNavel[0]);
+        offsets[1] = -(origin[1] + hand[1] + armNavel[1] - armHand[1] - bodyNavel[1]);
     }
     if (mapSpots.includes('handMove')) {
-        handMove[0] = -maps['handMove'][0];
-        handMove[1] = -maps['handMove'][1];
+        handMove[0] = maps['handMove'][0];
+        handMove[1] = maps['handMove'][1];
         if (partName == 'lHand') {
-            leftHandMove = handMove;
+            leftHandMove[0] = handMove[0];
+            leftHandMove[1] = handMove[1];
         }
+        console.log(origin + '   ' + handMove + '   ' + leftHandMove);
         offsets[0] = -(origin[0] + handMove[0] - leftHandMove[0]);
         offsets[1] = -(origin[1] + handMove[1] - leftHandMove[1]);
     }
     if (mapSpots.includes('navel')) {
-        navel[0] = -maps['navel'][0];
-        navel[1] = -maps['navel'][1];
+        navel[0] = maps['navel'][0];
+        navel[1] = maps['navel'][1];
         if (partName == 'arm') {
-            armNavel = navel;
+            armNavel[0] = navel[0];
+            armNavel[1] = navel[1];
         }
         if (partName == 'body') {
-            bodyNavel = navel;
+            bodyNavel[0] = navel[0];
+            bodyNavel[1] = navel[1];
         }
-        offsets[0] = origin[0] + navel[0] - bodyNavel[0];
-        offsets[1] = origin[1] + navel[1] - bodyNavel[1];
+        console.log(origin + '   ' + navel + '   ' + bodyNavel);
+        offsets[0] = -(origin[0] + navel[0] - bodyNavel[0]);
+        offsets[1] = -(origin[1] + navel[1] - bodyNavel[1]);
     }
     return offsets;
 }
